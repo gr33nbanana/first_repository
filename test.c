92477342cfdac4c00d9f58fe860fa5342b360af7
Kaloyan <k.pavlov1994@gmail.com>
2017-11-05 12:02:51 +0100

    Learn syntax sergio

--- test.c
+++ test.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
-# ADD comments to your code!!!!!!!!
+// ADD comments to your code!!!!!!!!
 int main(){
   int a = 10;
-
   do{
     printf("I will not conquer the world in the next %d years\n",a);
     do{
       printf("I will not conquer the world in the next %d years\n", a);
-      a = a+1;
-    }while(a<20);
-    a = a-15;
-  }while(a>0);
+      a = a + 1;
+    }while (a < 20);
+    a = a - 15;
+  }while (a > 0);
 
 }
 
